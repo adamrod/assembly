cb7ed04c83d0c6a31a57cf719cf864c9a13a6a3e
Adam Rodacki <amrodacki@gmail.com>
2020-05-06 17:50:50 +0200

    factorial nonrecursive version

---
+++ factorial_nonrecursive.s
@@ -0,0 +1,33 @@
+.section .data
+
+.section .text
+
+.globl _start
+.globl factorial
+
+_start:
+pushl $4
+call factorial
+addl $4, %esp
+movl %eax, %ebx
+movl $1, %eax
+int $0x80
+
+.type factorial,@function
+factorial:
+pushl %ebp
+movl %esp, %ebp
+movl 8(%ebp), %eax
+movl 8(%ebp), %ebx
+
+factorial_loop:
+cmpl $1, %ebx
+je end_factorial
+decl %ebx
+imull %ebx, %eax
+jmp factorial_loop
+
+end_factorial:
+movl %ebp, %esp
+popl %ebp
+ret
